21ab46a942eb440a731d10456c7e238256202bac
Saad Shahid <sshahid@illuminatics.local>
2019-06-27 15:10:25 +0500

    work on issues

---
+++ application-service/src/main/java/com/myKidGoal/controller/AreaController.java
@@ -0,0 +1,33 @@
+package com.myKidGoal.controller;
+
+import com.myKidGoal.model.Area;
+import com.myKidGoal.repository.AreaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/sms/areas")
+public class AreaController {
+
+    @Autowired
+    AreaRepository areaRepository;
+
+    @GetMapping("/search/all")
+    public Map<String,Object> allAreas() {
+        Map<String,Object> response = new HashMap<>();
+        Map<String,Object> res = new HashMap<>();
+        res.put("areas",areaRepository.findAll());
+        response.put("_embedded",res);
+        return response;
+
+    }
+
+    @PostMapping("/save")
+    public void saveArea(@RequestBody Area area) {
+        areaRepository.save(area);
+    }
+
+}
---
+++ application-service/src/main/java/com/myKidGoal/controller/ProfessionController.java
@@ -0,0 +1,33 @@
+package com.myKidGoal.controller;
+
+import com.myKidGoal.model.Profession;
+import com.myKidGoal.repository.ProfessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/sms/profession")
+public class ProfessionController {
+
+    @Autowired
+    ProfessionRepository professionRepository;
+
+    @GetMapping("/search/all")
+    public Map<String,Object> allProfession() {
+        Map<String,Object> response = new HashMap<>();
+        Map<String,Object> res = new HashMap<>();
+        res.put("profession",professionRepository.findAll());
+        response.put("_embedded",res);
+        return response;
+
+    }
+
+    @PostMapping("/save")
+    public void saveProfession(@RequestBody Profession profession) {
+        professionRepository.save(profession);
+    }
+
+}
