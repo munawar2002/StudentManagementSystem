e872a1c776e944ca67a31b01cb1148886adf9345
munawar2002 <munawar.ali2002@gmail.com>
2019-10-27 18:10:25 +0500

    Fixing the exception handler controller.

--- application-service/src/main/java/com/myKidGoal/exception/ControllerExceptionHandler.java
+++ application-service/src/main/java/com/myKidGoal/exception/ControllerExceptionHandler.java
@@ -3,6 +3,7 @@ package com.myKidGoal.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.logging.Logger;
@@ -29,6 +30,7 @@ public class ControllerExceptionHandler {
      */
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(value = RuntimeException.class)
+    @ResponseBody
     public Integer handleRuntimeException(RuntimeException e) {
         logger.severe("Exception occurred while performing task." + e);
         return HttpStatus.INTERNAL_SERVER_ERROR.value();
